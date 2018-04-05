e62106f1ee1ce28e758a44b693d20edf80c866e5
Ryan Manro <rmanro@gmail.com>
2018-04-05 12:03:36 -0700

    transform invert working

--- test/bitmap-transformer.test.js
+++ test/bitmap-transformer.test.js
@@ -1,15 +1,15 @@
 const assert = require('assert');
 const fs = require('fs');
-// const BitmapTransformer = require('../lib/bitmap-transformer');
-// const invert = require('../lib/invert-transform');
+const BitmapTransformer = require('../lib/bitmap-transformer');
+const invert = require('../lib/invert-transformer');
 
 describe('bitmap file transformer', () => {
     
     let buffer = null;
     beforeEach(() => {
-        // TODO: read './test/test-bitmap.bmp' into buffer variable
+        // TODOne: read './test/test-bitmap.bmp' into buffer variable
         // Okay to use `sync` file methods for now
-
+        buffer = fs.readFileSync('./test/test-bitmap.bmp');
         // TODO: If the functionality in this before test is same as 
         // other test, can you remove (extract) the duplication?
     });
