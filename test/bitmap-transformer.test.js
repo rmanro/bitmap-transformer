d82f86bd9ad1dbe2480016f673797d5179910cde
Carmen Perezchica <crperezchica@gmail.com>
2018-04-06 11:25:57 -0700

    added create method for bitmap transformer

--- test/bitmap-transformer.test.js
+++ test/bitmap-transformer.test.js
@@ -5,23 +5,33 @@ const invert = require('../lib/invert-transformer');
 
 describe('bitmap file transformer', () => {
     
-    let buffer = null;
-    beforeEach(() => {
+    // let buffer = null;
+    // beforeEach(() => {
 
-        buffer = fs.readFileSync('./test/test-bitmap.bmp');
+    //     buffer = fs.readFileSync('./test/test-bitmap.bmp');
 
+    // });
+
+    // it('test whole transform', () => {
+
+    //     const bitmap = new BitmapTransformer(buffer);
+
+    //     bitmap.transform(invert);
+
+
+    //     const expected = fs.readFileSync('./test/inverted-expected.bmp');
+    //     assert.deepEqual(bitmap.buffer, expected);
+
+
+    // });
+
+    it('test new transformer class', () => {
+        const filename = ('./test/test-bitmap.bmp');
+        
+        return BitmapTransformer.create(filename) 
+            .then (bitmap => {
+                console.log(bitmap);
+            }); 
     });
 
-    it('test whole transform', () => {
-
-        const bitmap = new BitmapTransformer(buffer);
-
-        bitmap.transform(invert);
-
-
-        const expected = fs.readFileSync('./test/inverted-expected.bmp');
-        assert.deepEqual(bitmap.buffer, expected);
-
-
-    });
 });
